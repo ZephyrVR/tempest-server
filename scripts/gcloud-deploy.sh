dc8c4017979a1f8c2e1db22a6a6a4bcedcc56e11
Thomas Gaubert <tpgaubert@gmail.com>
2017-03-11 22:28:02 -0600

    Back to the original attempt

--- scripts/gcloud-deploy.sh
+++ scripts/gcloud-deploy.sh
@@ -2,10 +2,10 @@
 
 # If on master and not a pull request, deploy to production
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-  gcloud compute ssh $GCLOUD_INSTANCE --zone $GCLOUD_ZONE -- "./deploy-prod.sh"
+  gcloud compute ssh $GCLOUD_INSTANCE --zone $GCLOUD_ZONE --command "./deploy-prod.sh"
 fi
 
 # If on dev and not a pull request, deploy to dev
 if [ "$TRAVIS_BRANCH" = "dev" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-  gcloud compute ssh $GCLOUD_INSTANCE --zone $GCLOUD_ZONE "pwd"
+  gcloud compute ssh $GCLOUD_INSTANCE --zone $GCLOUD_ZONE --command "./deploy-dev.sh"
 fi
