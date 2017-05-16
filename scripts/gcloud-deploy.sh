4b362c4c23d7983cbb8b173d933556f308f9516c
Thomas Gaubert <tpgaubert@gmail.com>
2017-05-15 22:54:29 -0500

    Update gcloud-deploy.sh for latest server configuration

--- scripts/gcloud-deploy.sh
+++ scripts/gcloud-deploy.sh
@@ -2,7 +2,7 @@
 
 # If on master and not a pull request, deploy to production
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-  gcloud compute ssh $GCLOUD_INSTANCE --zone $GCLOUD_ZONE --command "./deploy-prod.sh"
+  gcloud compute ssh $GCLOUD_INSTANCE --zone $GCLOUD_ZONE --command "/var/www/deploy.sh"
 fi
 
 gcloud compute instances remove-metadata $GCLOUD_INSTANCE --zone $GCLOUD_ZONE --keys ssh-keys
